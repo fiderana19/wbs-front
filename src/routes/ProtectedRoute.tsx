6b8eb595b409d8cdc5ff715bdb895cfda92f7605
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2025-01-31 15:14:05 +0300

    feat: protected route almost fixed

---
+++ src/routes/ProtectedRoute.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useAuth } from "../context/AuthContext";
+import { Navigate, Outlet } from "react-router-dom";
+
+const ProtectedRoute: React.FC = () => {
+    const { token, isAuthenticated } = useAuth();
+
+    if(!isAuthenticated) {
+        return <Navigate to="/" />
+    }
+    
+    if(isAuthenticated && (token !== null)) {
+        return <Outlet />
+    } else {
+        return <Navigate to="/unauthorized" />
+    }
+}
+
+export default ProtectedRoute;
