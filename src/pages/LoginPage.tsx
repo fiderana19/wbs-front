0696abe62d3d4dd58844413c5a4cff9813e065c4
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2025-01-31 12:22:01 +0300

    feat: Input almost done

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,18 +1,29 @@
+import { Link } from "react-router-dom";
+
 const LoginPage: React.FC = () => {
     return <div className="h-screen">
         <div className="flex h-full">
             <div className="w-2/3 bg-red-400">
                 niujnv
             </div>
-            <div className="w-1/3 bg-primary">
-                <div>CONNEXION</div>
-                <div className="w-full">
-                    <div className="mx-auto">
-                        <div className="text-xs">Identifiant</div>
-                        <input type="text" name="usrid" className="w-60 py-0.5 px-2 rounded bg-transparent border border-gray-500" />
+            <div className="w-1/3 bg-primary flex flex-col justify-center">
+                <div className="max-w-max mx-auto">
+                    <div className="my-4 text-center">CONNEXION</div>
+                    <div className="w-full">
+                        <div className="mx-auto my-1">
+                            <div className="text-xs">Identifiant</div>
+                            <input type="text" name="usrid" className="w-64 py-0.5 px-2 rounded bg-transparent border border-gray-500" />
+                        </div>
+                        <div className="mx-auto my-1">
+                            <div className="text-xs">Mot de passe</div>
+                            <input type="password" name="usrid" className="w-64 py-0.5 px-2 rounded bg-transparent border border-gray-500" />
+                        </div>
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white mx-auto font-latobold py-2 px-4 my-1 rounded w-64">Se connecter</button>
+                    </div>
+                    <div className="flex mt-10">
+                        Pas encore de compte? <Link to="/login"><div className="text-blue-600 underline mx-1">S'incrire</div></Link>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
