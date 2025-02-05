61ca7dd308bcdc3e9de09c13a2d39dd0f397eb7e
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2025-02-05 08:44:47 +0300

    feat: Initialized the login

--- src/pages/admin/LoginPage.tsx
+++ src/pages/admin/LoginPage.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import { LoginInterface } from "../../interfaces/Auth.interface";
 import { useAuth } from "../../context/AuthContext";
+import WbsLogo from '../../assets/image/wbs-logo.png'
+import { Link } from "react-router-dom";
 
 const LoginPage: React.FC = () => {
     const [loginCredentials, setLoginCredentials] = useState<LoginInterface>({ usrid: "", password: "" });
@@ -20,26 +21,33 @@ const LoginPage: React.FC = () => {
 
     return <div className="h-screen">
         <div className="flex h-full">
-            <div className="w-2/3 bg-red-400">
-                niujnv
-            </div>
-            <div className="w-1/3 bg-primary flex flex-col justify-center">
+            <div className="w-1/3 bg-primary flex flex-col justify-center relative">
+                <img src={WbsLogo} alt="Logo" className="absolute h-12 object-cover top-5 left-5" />
                 <div className="max-w-max mx-auto">
-                    <div className="my-4 text-center">CONNEXION</div>
+                    <div className="mb-10 text-center font-latobold text-2xl">Connexion</div>
                     <div className="w-full">
-                        <div className="mx-auto my-1">
+                        <div className="mx-auto my-3">
                             <div className="text-xs">Identifiant</div>
                             <input onChange={handleInputChange} type="text" name="usrid" className="w-64 py-1.5 px-2 rounded bg-transparent border border-gray-500" />
                         </div>
-                        <div className="mx-auto my-1">
+                        <div className="mx-auto my-3">
                             <div className="text-xs">Mot de passe</div>
                             <input onChange={handleInputChange} type="password" name="password" className="w-64 py-1.5 px-2 rounded bg-transparent border border-gray-500" />
                         </div>
                         <button onClick={loginSubmit} className="bg-blue-500 hover:bg-blue-700 text-white mx-auto font-latobold py-2 px-4 my-1 rounded w-64">Se connecter</button>
                     </div>
-                    <div className="flex mt-10">
-                        Pas encore de compte? <Link to="/login"><div className="text-blue-600 underline mx-1">S'incrire</div></Link>
-                    </div>
+                </div>
+            </div>
+            <div className="w-2/3 flex flex-col justify-center relative p-5">
+                <div className="absolute h-12 object-cover top-5 right-5 flex gap-4 items-center">
+                    <div>Pas encore de compte ?</div>
+                    <Link to="/" className="border hover:shadow-lg transition-all font-latobold py-1.5 px-2.5 my-1 rounded-full">S'inscrire</Link>
+                </div>
+                <div className="text-5xl">
+                    Bienvenue sur <span className="font-latobold">WBS-Caisse</span>
+                </div>
+                <div className="mt-4 text-xl">
+                    Application pour la gestion de caisse
                 </div>
             </div>
         </div>
